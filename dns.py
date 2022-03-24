dda1c086d4fb598d8065ce17cf32a9a1126ba66d
nobodyzhere <myclosestisa@gmail.com>
2022-03-25 01:06:05 +0500

    Pass category if in checked list

--- dns.py
+++ dns.py
@@ -7,21 +7,24 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 driver = webdriver.Chrome(service=Service("./src/chromedriver.exe"), options=chrome_options)
 links = []
+all_links = []
 
 def getChildLinks(driver, url, list):
-    driver.get(url)
-    time.sleep(0.2)
-    soup = BeautifulSoup(driver.page_source, 'lxml')
-    links = soup.find_all('a', {'class':['subcategory__item', 'subcategory__item', 'ui-link ui-link_blue']})
-    if len(links) > 0:
-        for e in links:
-            try:
-                getChildLinks(driver, 'https://www.dns-shop.ru'+e['href'], links)
-            except:
-                pass
-    else:
-        print('%s added to list'%url)
-        list.append(url)
+    if url not in all_links:
+        driver.get(url)
+        time.sleep(0.2)
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        links = soup.find_all('a', {'class':['subcategory__item', 'subcategory__item', 'ui-link ui-link_blue']})
+        if len(links) > 0:
+            for e in links:
+                try:
+                    getChildLinks(driver, 'https://www.dns-shop.ru'+e['href'], links)
+                except:
+                    pass
+        else:
+            print('%s added to list'%url)
+            list.append(url)
+        all_links.append(url)
 
 driver.get('https://www.dns-shop.ru/catalog/')
 soup = BeautifulSoup(driver.page_source, 'lxml')
