dea5156848149106b39a410f7bb1bb1c91e53805
nobodyknowsdotcom <myclosestisa@gmail.com>
2022-03-26 17:44:00 +0500

    Format and strip links

--- get_links.py
+++ get_links.py
@@ -9,29 +9,29 @@ driver = webdriver.Chrome(service=Service("./src/chromedriver.exe"), options=chr
 links = []
 all_links = []
 
-def getChildLinks(driver, url, list):
+def getLinks(driver, url, list):
     if url not in all_links:
-        try:
-            driver.get(url)
-        except:
-            pass
-        time.sleep(0.2)
+        driver.get(url)
         soup = BeautifulSoup(driver.page_source, 'lxml')
         links = soup.find_all('a', {'class':'subcategory__item'})
         if len(links) > 0:
             for e in links:
-                getChildLinks(driver, 'https://www.dns-shop.ru'+e['href'], list)
+                getLinks(driver, 'https://www.dns-shop.ru'+e['href'], list)
         else:
-            print('%s added to list'%url)
-            list.append(url)
+            if 'recipe' in url:
+                link = '/'.join(url.split('/')[:7])+'/'
+                list.append(link)
+            else: 
+                link = '/'.join(url.split('/')[:6])+'/'
+                list.append(link)
+            print(link)
     all_links.append(url)
 
 driver.get('https://www.dns-shop.ru/catalog/')
 soup = BeautifulSoup(driver.page_source, 'lxml')
 for e in soup.find_all('div', {'class': ['subcategory__item', 'subcategory__item_with-childs']}):
     category = e.find('a', {'class': 'subcategory__childs-item'})['href']
-    time.sleep(0.5)
-    getChildLinks(driver, 'https://www.dns-shop.ru'+category, links)
+    getLinks(driver, 'https://www.dns-shop.ru'+category, links)
 
 with open('categories.txt', 'w') as f:
     for item in links:
