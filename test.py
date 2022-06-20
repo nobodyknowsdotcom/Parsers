18eaaff13f3da0f79c52b90e13fe4490241b8eab
Dmitry Medvedev <myclosestisa@gmail.com>
2022-06-21 01:48:09 +0500

    Use AJAX to request JSON from Wb API

--- test.py
+++ test.py
@@ -1,12 +1,34 @@
+import json
 import time
+from urllib.request import Request
 import requests
-from bs4 import BeautifulSoup
+from time import sleep
 
-page = requests.get('https://www.wildberries.ru/catalog/zhenshchinam/odezhda/bluzki-i-rubashki?sort=popular&page=2', headers=)
-time.sleep(5)
-soup = BeautifulSoup(page.text, 'lxml')
+from seleniumwire import webdriver
 
-for product_card in soup.find_all('div', {'class':'product-card__wrapper'}):
-    name = product_card.find('span', {'class' : 'goods-name'}).get_text()
-    
-    print(name)
+driver = webdriver.Chrome()
+driver.set_window_position(2000, 2000)
+driver.get('https://www.wildberries.ru/catalog/zhenshchinam/odezhda/bluzki-i-rubashki?sort=popular&page=1&discount=30')
+
+time.sleep(2)
+
+# Access requests via the `requests` attribute
+url = ''
+for request in driver.requests:
+    if request.response:
+        if '/catalog?' in request.url:
+            url = request.url
+            print(request.url)
+
+driver.close()
+
+r = requests.get(url)
+r.encoding = 'utf-8'
+deserialized = json.loads(r.text)
+
+for e in deserialized['data']['products']:
+    name = e['name']
+    brand = e['brand']
+    price = (int) (e['priceU'] / 100)
+    discount_price = (int) (e['salePriceU'] / 100)
+    print(name, brand, price, discount_price, sep=' ')
