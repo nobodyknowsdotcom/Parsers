9b0132e1e5e438c6fc4b2f85c8358bfe43eb0084
Dmitry Medvedev <myclosestisa@gmail.com>
2022-07-12 01:34:12 +0500

    Parse driver content and write all items to json

--- parser.py
+++ parser.py
@@ -1,18 +1,94 @@
+import json
 import re
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import selenium
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
 driver = webdriver.Chrome(service=Service("./chromedriver.exe"), options=options)
 
+data = []
 with open('./urls.txt', 'r') as f:
     urls = f.readlines()
 
-for url in urls:
-    driver.get(url)
-    time.sleep(1.5)
+def parse_soup(soup: BeautifulSoup):
+    name = soup.select_one('h1').text.strip()
+    title_seconds = soup.select_one('.meta span.text').text.strip()
+    preview_pictures = soup.find('a', class_='img-container').get('href')
+    detail_picture = soup.find('a', class_='img-container').get('href')
+    desc = soup.find('div', class_='desc').find('div', class_='description-block collapse-content')
+    notes_desc = soup.select_one('.articles-container.articles-col.collapsible-block.notes.opened-half').select('.description-block')
+
+    if notes_desc != []:
+        notes_color = notes_desc[0].find("p").text
+        notes_vkus = notes_desc[1].find("p").text
+        notes_aromat = notes_desc[2].find("p").text
+        notes_gastr = notes_desc[3].find("p").text
+    else:
+        notes_color = ''
+        notes_vkus = ''
+        notes_aromat = ''
+        notes_gastr = ''
+    techs = {}
+    select = soup.select('ul.list-description li')
+    
+    new_rows = []
+    for row in select:
+        cols = row.text.strip().split(':')
+        if (len(row) != 0):
+            if (len(row) != 1):
+                tech_name = row.find('span', class_="name").text.strip(':')
+                tech_value = row
+                tech_val = tech_value.span.decompose()
+                tech_val2 = tech_value.text.strip()
+                arr = [tech_name, tech_val2]
+                # print(val, value.text.strip(','))
+                new_rows.append(arr)
+
+    for new_row in new_rows:
+        techs[new_row[0]] = new_row[1].strip()
+
+    item = {
+        'name': name,
+        'name_tr': title_seconds,
+        'preview_pictures': preview_pictures,
+        'detail_picture': detail_picture,
+        'desc': str(desc),
+        'notes_color': str(notes_color),
+        'notes_vkus': str(notes_vkus),
+        'notes_aromat': str(notes_aromat),
+        'notes_gastr': str(notes_gastr),
+        'techs': techs,
+        'url': url,
+    }
+    return item
+
+def dump_to_json(filename, data, **kwargs):
+    kwargs.setdefault('ensure_ascii', False)
+    kwargs.setdefault('indent', 1)
+    
+    with open(filename, 'w') as f:
+        json.dump(data, f, **kwargs)
+
+for url in urls[:100]:
+    try:
+        driver.get(url)
+        time.sleep(2)
+    except:
+        time.sleep(5)
+        continue
+    try:
+        item = parse_soup(BeautifulSoup(driver.page_source, 'lxml'))
+    except AttributeError:
+        continue
+    data.append(item)
+    print(item, sep='\n')
+
+dump_to_json('winestyle.json', data)
+driver.close()
+
 
 
