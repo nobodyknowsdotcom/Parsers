d34531aeac5b2e744ada98a8a1f5c72751b39f0f
Dmitry Medvedev <myclosestisa@gmail.com>
2022-07-19 16:49:53 +0500

    Parse full category name

--- get_categories.py
+++ get_categories.py
@@ -1,31 +1,40 @@
 import pickle
-import json
-import time
 import requests
 import pickle
 
-def expand_catalog(full_catalog):
+def expand_catalog(full_catalog, prefix=''):
     result = {}
 
     for category in full_catalog:
-
         try:
             if category['childs']:
-                result.update(expand_catalog(category['childs']))
+                try:
+                    if prefix == '':
+                        result.update(expand_catalog(category['childs'], category['seo']))
+                    else:
+                        result.update(expand_catalog(category['childs'], prefix + '/' + category['seo']))
+                except KeyError:
+                    if prefix == '':
+                        result.update(expand_catalog(category['childs'], category['name']))
+                    else:
+                        result.update(expand_catalog(category['childs'], prefix + '/' + category['name']))
             else:
-                result[category['query']] = category['seo']
+                result[category['query']] = prefix + '/' + category['seo']
         except KeyError:
             try:
-                result[category['query']] =  category['seo']
+                result[category['query']] =  prefix + '/' + category['seo']
             except KeyError:
                 try:
-                    result[category['query']] =  category['name']
+                    result[category['query']] =  prefix + '/' + category['name']
                 except KeyError:
                     print(category)
     return result
 
 r = requests.get('https://www.wildberries.ru/webapi/menu/main-menu-ru-ru.json')
 categories = expand_catalog(r.json())
+for (key, val) in categories.items():
+    print(key, val)
+
 print(len(categories))
 
 with open('categories.pickle', 'wb') as handle:
